709e6859d043d4ff882ef8b0348d326ab287d014
Saeed-Paris <saeed.paris.0026@gmail.com>
2022-06-16 11:17:55 +0430

    add mutations and product() in homePage

--- src/store/index.js
+++ src/store/index.js
@@ -3,17 +3,28 @@ import { createStore } from "vuex";
 export default createStore({
   state: {
     products: [
-      { id: 1, price: 1000, isAvailable: true, title: "Book" },
-      { id: 2, price: 2000, isAvailable: false, title: "Apple" },
-      { id: 3, price: 3000, isAvailable: true, title: "Banana" },
+      { id: 1, price: 1000, isAvailable: null, title: "Book", quantity: 5 },
+      { id: 2, price: 2000, isAvailable: null, title: "Apple", quantity: 0 },
+      { id: 3, price: 3000, isAvailable: null, title: "Banana", quantity: 3 },
     ],
   },
   getters: {
-    availableBooks: (state) => {
+    availableProducts: (state) => {
       return state.products.filter((product) => product.isAvailable);
     },
   },
-  mutations: {},
+  mutations: {
+    toggleAvailability(state, product) {
+      const index = state.products.findIndex((v) => v.id === product.id);
+      state.products[index].isAvailable = !state.products[index].isAvailable;
+      // state.products[index].quantity+=
+    },
+    IsAvailableFunc(state, product) {
+      state.products.forEach((p) => 
+        p.quantity > 0 ? (p.isAvailable = true) : (p.isAvailable = false)
+      );
+    },
+  },
   actions: {},
   modules: {},
 });
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,17 @@
-<template >
-    <div>
-       
-    </div> 
+<template>
+  <section>
+    <h1>{{products}}</h1>
+  </section>
 </template>
 <script>
 export default {
-   
-}
+  computed: {
+    products() {
+
+        // this.$store.mutations.IsAvailableFunc;
+      return this.$store.getters.availableProducts;
+    },
+  },
+};
 </script>
-<style lang="">
-    
-</style>
+<style lang=""></style>
