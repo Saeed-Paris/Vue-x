7b9dfdeaa487562f99ad0bc0be6fd59a6d77e2d1
Saeed-Paris <saeed.paris.0026@gmail.com>
2022-06-16 10:38:59 +0430

    add state and getters for products

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,19 @@
-import { createStore } from 'vuex'
+import { createStore } from "vuex";
 
 export default createStore({
   state: {
+    products: [
+      { id: 1, price: 1000, isAvailable: true, title: "Book" },
+      { id: 2, price: 2000, isAvailable: false, title: "Apple" },
+      { id: 3, price: 3000, isAvailable: true, title: "Banana" },
+    ],
   },
   getters: {
+    availableBooks: (state) => {
+      return state.products.filter((product) => product.isAvailable);
+    },
   },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
-})
+  mutations: {},
+  actions: {},
+  modules: {},
+});
